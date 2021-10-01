c0abff6af6e42032e67b61c3a7b10db4eed6ce37
WilderGitHub <wilder_serdan@hotmail.com>
2021-10-01 15:58:12 -0400

    solo grid con dos sectores

--- prueba1.R
+++ prueba1.R
@@ -34,10 +34,14 @@ combenado%>%
 ##  2  ## LINEAS POR SECTORES
 #Importamos el excel
 BDTesis<-import("BDTesis4.xlsx")%>% as_tibble
-BDTesis
+BDfiltro<-BDTesis%>%
+  filter(Categoria!="IC")
+
+BDfiltro
 #TotalFinanciamiento <- aggregate(BDTesis$Valor,list(BDTesis$Periodo), FUN=sum)
-TotalFinanciamiento <-aggregate(Valor ~ Periodo, data = BDTesis, FUN = sum, na.rm = TRUE)
+TotalFinanciamiento <-aggregate(Valor ~ Periodo+Categoria+SectorINECorto, data = BDfiltro, FUN = sum, na.rm = TRUE)
 ggplot(TotalFinanciamiento,aes(Periodo,Valor, group=1))+
-  geom_line(color = "steelblue",size=1)
+  geom_line(color = "steelblue",size=1)+
+  facet_grid(SectorINECorto~Categoria)
   
 
