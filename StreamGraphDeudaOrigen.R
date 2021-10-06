8fec1d27b9f7c5209a5c7ec60b558fd40d984db4
WilderGitHub <wilder_serdan@hotmail.com>
2021-10-06 15:41:59 -0400

    nya funciona deuda

--- StreamGraphDeudaOrigen.R
+++ StreamGraphDeudaOrigen.R
@@ -2,20 +2,20 @@
 pacman::p_load(pacman,party,rio,tidyverse) 
 library(ggstream)
 #importamos los datos oe
-df1<-import("DeudaOrigen.xlsx") %>% as_tibble()
+df<-import("DeudaOrigen.xlsx") %>% as_tibble()
 #Filtramos el pais y botamos otros que se estaba repitiendo
-df1
-dfacreedor<-df1%>%
-  filter(Acreedor=="China")
-dfacreedor
+dfacreedor<-df%>%
+  filter(Acreedor=="CAF")
+
 #definimos los colores
 cols <- c('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd')
 
 #dibujamos el streamgraph
-ggplot(dfacreedor, aes(x = Periodo, y = Valor, fill = Sector)) +
+#OJO ESTE PERIODO DEBE SER CONTINUO OE, ESO ESTABA ARRUINANDO
+ggplot(dfacreedor, aes(x = Pperiodo, y = Valor, fill = Sector)) +
   geom_stream() +
   scale_fill_manual(values = cols)+
-  labs(title = "Deuda de CAF",
+  labs(title = "IED de España",
        
        subtitle = "(En millones de $us)",
        caption  = "Fuente: BCB"
