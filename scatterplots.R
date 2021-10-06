ba6a95f0acd4aba7f3440386eb1ea85152d4b070
WilderGithub <wilder_serdan@hotmail.com>
2021-10-05 23:50:48 -0400

    ultima del dia 2

--- scatterplots.R
+++ scatterplots.R
@@ -4,8 +4,8 @@ BDsectores<-import("Tidy05oct.xlsx")%>% as_tibble
 head(BDsectores)
 ##  1  ## SCATTERPLOT POR SECTORES
 #Filtramos el sector
-BDfiltrado<-BDsectores%>%
-  filter(SectorTesis=="PetroleoGas")
+BDfiltrado<-BDsectores#%>%
+  #filter(SectorTesis=="PetroleoGas")
 head(BDfiltrado)
 
 #eLa variable lo volvemos tidy
@@ -14,5 +14,8 @@ BDfiltradoWide<-BDfiltrado%>%
   pivot_wider(names_from = Variable, values_from = Valor)
   #pivot_longer(cols = !Periodo, names_to = "SectorINECorto",values_to = "value")
 
-pairs(BDfiltradoWide[,5:9])
+pairs(BDfiltradoWide[,5:9],cex=1
+      #,col = BDfiltradoWide$SectorTesis,
+      ,lower.panel=NULL)
+
 
