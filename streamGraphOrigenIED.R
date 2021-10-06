5f1002147db730a9085418dd79e1224f15ed7a47
WilderGitHub <wilder_serdan@hotmail.com>
2021-10-06 13:37:41 -0400

    yasta funcionando

---
+++ streamGraphOrigenIED.R
@@ -0,0 +1,22 @@
+#Las librerias de rigor
+pacman::p_load(pacman,party,rio,tidyverse) 
+library(ggstream)
+#importamos los datos oe
+df<-import("IEDorigen.xlsx") %>% as_tibble()
+#Filtramos el pais y botamos otros que se estaba repitiendo
+dfpais<-df%>%
+  filter(Pais=="Brasil" & Sector!="Otros")
+
+#definimos los colores
+cols <- c('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd')
+
+#dibujamos el streamgraph
+ggplot(dfpais, aes(x = Periodo, y = Valor, fill = Sector)) +
+  geom_stream() +
+  scale_fill_manual(values = cols)+
+  labs(title = "IED de España",
+       
+       subtitle = "(En millones de $us)",
+       caption  = "Fuente: BCB"
+  )
+# + geom_stream_label(aes(label = Sector))
