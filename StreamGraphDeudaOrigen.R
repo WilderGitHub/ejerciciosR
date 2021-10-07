73747cafbbb128580ad662d3489c8c1c607118b4
WilderGithub <wilder_serdan@hotmail.com>
2021-10-06 22:52:41 -0400

    tiene dos graficos total y por sector

--- StreamGraphDeudaOrigen.R
+++ StreamGraphDeudaOrigen.R
@@ -3,6 +3,7 @@ pacman::p_load(pacman,party,rio,tidyverse)
 #install("ggstream")
 install.packages("ggstream")
 library(ggstream)
+#1. por sector===============================================
 #importamos los datos oe
 df<-import("DeudaOrigen.xlsx") %>% as_tibble()
 #Filtramos el pais y botamos otros que se estaba repitiendo
@@ -23,3 +24,24 @@ ggplot(dfacreedor, aes(x = Periodo, y = Valor, fill = Sector)) +
        caption  = "Fuente: BCB"
   )
 # + geom_stream_label(aes(label = Sector))
+
+#2. agregado por pais  ======================
+#importamos los datos oe
+df<-import("DeudaOrigen.xlsx") %>% as_tibble()
+#Filtramos el pais y botamos otros que se estaba repitiendo
+dfacreedor<-df#%>%
+#  filter(Acreedor=="CAF")
+TotalAcreedores <-aggregate(Valor ~ Periodo+Acreedor, data = dfacreedor, FUN = sum, na.rm = TRUE)
+#definimos los colores
+cols <- c('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd')
+
+#dibujamos el streamgraph
+#OJO ESTE PERIODO DEBE SER CONTINUO OE, ESO ESTABA ARRUINANDO
+ggplot(TotalAcreedores, aes(x = Periodo, y = Valor, fill = Acreedor)) +
+  geom_stream() +
+  scale_fill_manual(values = cols)+
+  labs(title = "Desembolsos por pais",
+       
+       subtitle = "(En millones de $us)",
+       caption  = "Fuente: BCB"
+  )
