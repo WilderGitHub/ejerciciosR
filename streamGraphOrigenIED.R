d1b342471dfbc58be476b508bcd4d54e27a88af9
WilderGitHub <wilder_serdan@hotmail.com>
2021-10-25 17:34:29 -0400

    mas graficos

--- streamGraphOrigenIED.R
+++ streamGraphOrigenIED.R
@@ -2,19 +2,58 @@
 #Las librerias de rigor
 pacman::p_load(pacman,party,rio,tidyverse) 
 library(ggstream)
+
+#1. por pais dados un Sector=====================================
 #importamos los datos oe
 df<-import("IEDorigen.xlsx") %>% as_tibble()
-#Filtramos el pais y botamos otros que se estaba repitiendo
+
+#Filtramos el sector y botamos otros que se estaba repitiendo
 dfpais<-df%>%
-  filter(Pais=="Estados Unidos" & Sector!="Otros")
-
+  filter(Sector=="Comercio" )
 #definimos los colores
-#cols <- c("#264653","#A69B4E",'#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd')
 cols <- c("#875B52","#E76F51", "#8FA078", "#228176", "#E9C46A", "#59804D", "#F4A261", "#264653", "#BB8865", "#A69B4E", "#65453E", "#E24D28", "#73835D", "#11403B", "#A07918", "#354D2E", "#EF7A1A", "#47829A", "#8E5E3E", "#6F6734", "#902C14", "#3F4833", "#7EDDD3", "#864109", "#274754")
+#dibujamos el streamgraph
+ggplot(dfpais, aes(x = Periodo, y = Valor, fill = Pais)) +
+  geom_stream() +
+  scale_fill_manual(values = cols)#+
 
+#2. por sector dado un Pais====================================  
+#importamos los datos oe
+df<-import("IEDorigen.xlsx") %>% as_tibble()
+
+#Filtramos el sector y botamos otros que se estaba repitiendo
+dfpais<-df%>%
+  filter(Pais=="Brasil" )
+#definimos los colores
+cols <- c("#875B52","#E76F51", "#8FA078", "#228176", "#E9C46A", "#59804D", "#F4A261", "#264653", "#BB8865", "#A69B4E", "#65453E", "#E24D28", "#73835D", "#11403B", "#A07918", "#354D2E", "#EF7A1A", "#47829A", "#8E5E3E", "#6F6734", "#902C14", "#3F4833", "#7EDDD3", "#864109", "#274754")
 #dibujamos el streamgraph
 ggplot(dfpais, aes(x = Periodo, y = Valor, fill = Sector)) +
   geom_stream() +
   scale_fill_manual(values = cols)#+
-  
+
+#  3. por sector de todos los paises====================================  
+#importamos los datos oe
+df<-import("IEDorigen.xlsx") %>% as_tibble()
+
+# Agregamos los sectores
+TotalInversores <-aggregate(Valor ~ Periodo+Sector, data = df, FUN = sum, na.rm = TRUE)
+#definimos los colores
+cols <- c("#875B52","#E76F51", "#8FA078", "#228176", "#E9C46A", "#59804D", "#F4A261", "#264653", "#BB8865", "#A69B4E", "#65453E", "#E24D28", "#73835D", "#11403B", "#A07918", "#354D2E", "#EF7A1A", "#47829A", "#8E5E3E", "#6F6734", "#902C14", "#3F4833", "#7EDDD3", "#864109", "#274754")
+#dibujamos el streamgraph
+ggplot(TotalInversores, aes(x = Periodo, y = Valor, fill = Sector)) +
+  geom_stream() +
+  scale_fill_manual(values = cols)#+
+
+#  4. por pais de todos los sectores====================================  
+#importamos los datos oe
+df<-import("IEDorigen.xlsx") %>% as_tibble()
+
+# Agregamos los sectores
+TotalSectores <-aggregate(Valor ~ Periodo+Pais, data = df, FUN = sum, na.rm = TRUE)
+#definimos los colores
+cols <- c("#875B52","#E76F51", "#8FA078", "#228176", "#E9C46A", "#59804D", "#F4A261", "#264653", "#BB8865", "#A69B4E", "#65453E", "#E24D28", "#73835D", "#11403B", "#A07918", "#354D2E", "#EF7A1A", "#47829A", "#8E5E3E", "#6F6734", "#902C14", "#3F4833", "#7EDDD3", "#864109", "#274754")
+#dibujamos el streamgraph
+ggplot(TotalSectores, aes(x = Periodo, y = Valor, fill = Pais)) +
+  geom_stream() +
+  scale_fill_manual(values = cols)#+
 
